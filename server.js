d591926ddecacb592f3d37f29fced9def1eca1f8
dkshith007 <annedeekshith@gmail.com>
2024-03-02 16:37:13 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
 
 const hotelRouter = require("./routes/hotel.router");
 const dataimportrouter = require("./routes/dataimport.router");
@@ -16,7 +17,7 @@ const app = express();
 const port = 3500;
 
 
-
+app.use(cors())
 app.use(express.json());
 connectDB();
 
